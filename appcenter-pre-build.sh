1e0f260736a73a146735a40b35f49ab22d1cffd7
Bruno Henn <brunofhenn@gmail.com>
2018-12-04 16:48:36 -0500

    prebuild script changed

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Creates an .env from ENV variables for use with react-native-config
-ENV_WHITELIST=${ENV_WHITELIST:-"^RN"}
+ENV_WHITELIST=${ENV_WHITELIST:-"^FIREBASE"}
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env
