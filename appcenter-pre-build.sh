caf39c2c0d5859ebcef46e6cabbeed010575a758
Bruno Henn <brunofhenn@gmail.com>
2020-04-29 18:39:11 -0400

    Removed old code to be replaced for the new version

--- appcenter-pre-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Creates an .env from ENV variables for use with react-native-config
-ENV_WHITELIST=${ENV_WHITELIST:-"^FIREBASE"}
-printf "Creating an .env file with the following whitelist:\n"
-printf "%s\n\n" $ENV_WHITELIST
-set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env
-printf "\n.env created with contents:\n"
-cat .env
--- index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { YellowBox } from "react-native";
-
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
-
-AppRegistry.registerComponent('SpendCA', () => App);
